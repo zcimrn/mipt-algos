63e4b961ba76d7fe812d8cd5197f695d17460d17
zcimrn <zcimrn@ya.ru>
2021-10-22 19:31:57 +0500

    fix 3_4 solution

--- module01/3_4/main.cpp
+++ module01/3_4/main.cpp
@@ -1,26 +1,47 @@
+// https://contest.yandex.com/contest/29755/run-report/55225171
+
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> a(n + 1);
-    for (int i = 0; i < n && cin >> a[i]; i++);
-    a[n] = 1e9;
-    cin >> n;
-    for (int x; cin >> x;) {
+std::vector<int> solve(std::vector<int>& a, std::vector<int>& b) {
+    a.push_back(1e9);
+    std::vector<int> ans(b.size());
+    for (int i = 0; i < b.size(); i++) {
         int l = 0, r = a.size() - 1;
-        for (int m = (l + r) / 2; r - l > 1; m = (l + r) / 2) {
-            if (a[m] > x) {
+        for (int m = (l + r) / 2; l + 1 < r; m = (l + r) / 2) {
+            if (a[m] > b[i]) {
                 r = m;
             }
             else {
                 l = m;
             }
         }
-        cout << (x - a[l] <= a[r] - x ? l : r) << ' ';
+        if (b[i] - a[l] <= a[r] - b[i]) {
+            ans[i] = l;
+        }
+        else {
+            ans[i] = r;
+        }
     }
-    cout << endl;
+    return ans;
 }
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    std::cin >> n;
+    std::vector<int> b(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> b[i];
+    }
+    for (auto i : solve(a, b)) {
+        std::cout << i << ' ';
+    }
+    std::cout << std::endl;
+    return 0;
+}
+
