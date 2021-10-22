879b081e034f3caa0f35e2f7acd4d83eb4c9d57e
zcimrn <zcimrn@ya.ru>
2021-10-22 19:01:07 +0500

    fix 2_2 solution

--- module01/2_2/main.cpp
+++ module01/2_2/main.cpp
@@ -1,19 +1,28 @@
-#include <iostream>
+// https://contest.yandex.com/contest/29755/run-report/55220563
 
-using namespace std;
+#include <iostream>
+#include <vector>
+
+float solve(std::vector<std::pair<int, int>>& a) {
+    for (int i = 1; i < a.size(); i++) {
+        a[i].first -= a[0].first;
+        a[i].second -= a[0].second;
+    }
+    int ans = 0;
+    for (int i = 1; i < a.size() - 1; i++) {
+        ans += a[i].first * a[i + 1].second - a[i].second * a[i + 1].first;
+    }
+    return static_cast<float>(ans) / -2;
+}
 
 int main() {
-    int n, x, y, xa, ya;
-    cin >> n >> x >> y >> xa >> ya;
-    xa -= x;
-    ya -= y;
-    int ans = 0;
-    for (int xb, yb; cin >> xb >> yb;) {
-        xb -= x;
-        yb -= y;
-        ans += xb * ya - yb * xa;
-        xa = xb;
-        ya = yb;
+    int n;
+    std::cin >> n;
+    std::vector<std::pair<int, int>> a(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i].first >> a[i].second;
     }
-    cout << (long double)ans / 2 << endl;
+    std::cout << solve(a) << std::endl;
+    return 0;
 }
+
