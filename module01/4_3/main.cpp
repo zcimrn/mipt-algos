b66c94af83ce9da12a7606ff6536a605201d1db7
zcimrn <zcimrn@ya.ru>
2021-10-22 19:41:30 +0500

    fix 4_3 solution

--- module01/4_3/main.cpp
+++ module01/4_3/main.cpp
@@ -1,19 +1,20 @@
+// https://contest.yandex.com/contest/29755/run-report/55227352
+
 #include <iostream>
+#include <vector>
 #include <map>
 #include <set>
 
-using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-    map<int, int> m;
-    for (int x; cin >> x; m[x]++);
-    set<pair<int, int>> s;
-    for (auto p: m) {
+int solve(std::vector<int>& a) {
+    std::map<int, int> m;
+    for (auto x : a) {
+        m[x]++;
+    }
+    std::set<std::pair<int, int>> s;
+    for (auto p : m) {
         s.insert({p.second, p.first});
     }
-    for (pair <int, int> a, b; s.size() > 1;) {
+    for (std::pair<int, int> a, b; s.size() > 1;) {
         a = *s.rbegin();
         s.erase(a);
         b = *s.rbegin();
@@ -26,8 +27,19 @@ int main() {
         }
     }
     if (s.size() == 0) {
-        cout << 0 << endl;
         return 0;
     }
-    cout << s.begin()->first << endl;
+    return s.begin()->first;
 }
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    std::cout << solve(a) << std::endl;
+    return 0;
+}
+
