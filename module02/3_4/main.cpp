90471a93f6e7d593d57a73a133e35667e673456e
zcimrn <zcimrn@ya.ru>
2021-10-30 18:33:00 +0500

    fix 02/3_4 solution

--- module02/3_4/main.cpp
+++ module02/3_4/main.cpp
@@ -1,4 +1,4 @@
-// https://contest.yandex.com/contest/30914/run-report/55608994
+// https://contest.yandex.com/contest/30914/run-report/55687341
 
 #include <iostream>
 #include <vector>
@@ -16,17 +16,17 @@ int partition(std::vector<int>& a, int l, int r) {
     return i + 1;
 }
 
-int solve(std::vector<int>& a, int k) {
+int get_kth_order_stat(std::vector<int>& a, int k) {
     for (int l = 0, r = a.size(); r - l > 1;) {
-        int p = partition(a, l, r);
-        if (p == k) {
-            return a[p];
+        int pivot = partition(a, l, r);
+        if (pivot == k) {
+            return a[pivot];
         }
-        if (p < k) {
-            l = p;
+        if (pivot < k) {
+            l = pivot;
         }
         else {
-            r = p;
+            r = pivot;
         }
     }
     return a[k];
@@ -38,7 +38,7 @@ int main() {
     std::vector<int> a(n);
     for (int i = 0; i < n && std::cin >> a[i]; i++);
     srand(time(0));
-    std::cout << solve(a, k) << std::endl;
+    std::cout << get_kth_order_stat(a, k) << std::endl;
     return 0;
 }
 
