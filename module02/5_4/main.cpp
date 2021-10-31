cc0d946ff75edfe0c532b7ff41f49e4ee75bd460
zcimrn <zcimrn@ya.ru>
2021-10-31 21:37:18 +0500

    fix 02/5_4 solution

--- module02/5_4/main.cpp
+++ module02/5_4/main.cpp
@@ -1,33 +1,34 @@
-// https://contest.yandex.com/contest/30914/run-report/55735133
+// https://contest.yandex.com/contest/30914/run-report/55735647
 
 #include <iostream>
 #include <vector>
 #include <stack>
 
 struct frame {
-    int l = 0, r = 0;
+    size_t l = 0, r = 0;
     unsigned long long mask = 0;
 
-    frame(int _l, int _r, unsigned long long _mask) : l {_l}, r {_r}, mask {_mask} {}
+    frame(size_t l, size_t r, unsigned long long mask) : l {l}, r {r}, mask {mask} {}
 };
 
-int partition(std::vector<unsigned long long>& a, int l, int r, unsigned long long mask) {
-    for (int i = l; i < r; i++) {
-        if (!(a[i] & mask)) {
-            std::swap(a[i], a[l]);
-            l++;
+size_t partition(std::vector<unsigned long long>& a, frame& frame) {
+    size_t pivot = frame.l;
+    for (size_t i = pivot; i < frame.r; i++) {
+        if (!(a[i] & frame.mask)) {
+            std::swap(a[i], a[pivot]);
+            pivot++;
         }
     }
-    return l;
+    return pivot;
 }
 
 void super_sort(std::vector<unsigned long long>& a) {
     std::stack<frame> frames;
-    frames.push({0, static_cast<int>(a.size()), 1ull << 63});
+    frames.push({0, a.size(), 1ull << 63});
     for (; frames.size();) {
         auto frame = frames.top();
         frames.pop();
-        int pivot = partition(a, frame.l, frame.r, frame.mask);
+        size_t pivot = partition(a, frame);
         if (pivot - frame.l > 1 && frame.mask > 1) {
             frames.push({frame.l, pivot, frame.mask >> 1});
         }
